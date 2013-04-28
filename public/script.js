fa4f834a638c60b246f18ea2ec3d2919351fdf76
Niall Ryan <ryann4@tcd.ie>
2013-04-28 22:25:03 +0100

    Removed Bootstrap, added some styling to Index, added basic AJAX functionality.

--- public/script.js
+++ public/script.js
@@ -5,3 +5,25 @@
  * Time: 22:35
  * To change this template use File | Settings | File Templates.
  */
+
+$(document).ready(function() {
+
+    // taken from http://stackoverflow.com/questions/7861150/jquery-fade-in-onload
+    $("#menu").hide(0).delay(500).fadeIn(3000);
+
+
+    $(".ajax").on("click", function(e) {
+        e.preventDefault();
+
+        var url = $(this).attr("href");
+
+        $.get(url, function(response) {
+            $("#main").html(response);
+        });
+
+    });
+
+});
+
+
+
