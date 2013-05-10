c05670e5efff5db3f5fc8377edaa6ddc7f597552
Niall Ryan <ryann4@tcd.ie>
2013-05-10 20:05:55 +0100

    Tweaked styling, fixed bugs.

--- public/quiz.js
+++ public/quiz.js
@@ -4,16 +4,21 @@ var ahh = document.getElementsByTagName("audio")[2];
 
 $("#start").on("click", function() {
     $(this).toggle();
-    $("#qna").toggle().animate({width: "1050px", height: "500px"}, 1000);
-    $("#timer").css("background", "white").animate({width: "1050px"}, 1000).animate({width: "10px"}, 10000, 'linear');
+    $("#qna").toggle().animate({width: "1050px", height: "350px"}, 1000);
+    $("#timer").css("background", "lightblue").animate({width: "1050px"}, 1000).animate({width: "10px"}, 10000, 'linear');
     music.play();
 
     var questions = [
-        { "text": "Question 1", "answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5"], "correct": "Answer 1" },
-        { "text": "Question 2", "answers": ["Answer A", "Answer B", "Answer C", "Answer D", "Answer E"], "correct": "Answer B" },
-        { "text": "Question 3", "answers": ["Answer F", "Answer G", "Answer H", "Answer I", "Answer J"], "correct": "Answer H" },
-        { "text": "Question 4", "answers": ["Answer K", "Answer L", "Answer M", "Answer N", "Answer O"], "correct": "Answer N" },
-        { "text": "Question 5", "answers": ["Answer P", "Answer Q", "Answer R", "Answer S", "Answer T"], "correct": "Answer T" }
+        { "text": "How many sharps are in the key of C?", "answers": ["1", "0", "3", "4", "355"], "correct": "0" },
+        { "text": "What are the Treble Clef line notes?", "answers": ["EGBDF", "EGCBA", "GBDFA", "FACEB", "CAGEC"], "correct": "EGBDF" },
+        { "text": "What are the Treble Clef space notes?", "answers": ["ACEG", "CAGE", "FABC", "GCEA", "FACE"], "correct": "FACE" },
+        { "text": "What are the Bass Clef space notes?", "answers": ["BCEA", "GCDA", "EADG", "ACEG", "BACE"], "correct": "ACEG" },
+        { "text": "What are the Bass Clef line notes?", "answers": ["GDFTA", "FGABE", "ABCDE", "FACBG", "GBDFA"], "correct": "GBDFA" },
+        { "text": "How many sharps are in the key of A?", "answers": ["2", "9", "4", "3", "100000"], "correct": "3" },
+        { "text": "How many keys are usually on a standard piano keyboard?", "answers": ["88", "6", "1000932", "77", "255"], "correct": "88" },
+        { "text": "How many sharps are in the key of A minor?", "answers": ["7", "0", "3", "4", "1"], "correct": "1" },
+        { "text": "What is the airspeed velocity of an unladen European swallow in MPH?", "answers": ["88", "6", "3", "24", "355"], "correct": "24" },
+        { "text": "How many sharps are in the key of G?", "answers": ["9", "2", "3", "4", "1"], "correct": "1" }
     ];
 
     var score = 0;
@@ -59,6 +64,7 @@ $("#start").on("click", function() {
                 $("#timer").stop();
                 $("#question").text("Quiz over.");
                 $("#answers").empty();
+                $("#qna").animate({height: "50px"}, 1000);
                 $("#try_again").text("Restart the Quiz").toggle();
                 clearInterval(timer_interval);
             }
@@ -73,6 +79,7 @@ $("#start").on("click", function() {
     var wrong_answer = (function() {
         $("#question").text("You picked the wrong answer or the time ran out. Bummer.");
         $("#answers").empty();
+        $("#qna").animate({height: "50px"}, 1000);
         $("#try_again").toggle();
         clearInterval(timer_interval)
         music.pause();
@@ -97,6 +104,7 @@ $("#start").on("click", function() {
         $("#score").text(score);
         timer = max_timer;
         timer_interval = setInterval(timer_function, 1000);
+        $("#qna").animate({width: "1050px", height: "350px"}, 1000);
         $("#timer").css("width", "1050px").delay(1000).animate({width: "10px"}, 10000, 'linear');
         show_question();
     });
